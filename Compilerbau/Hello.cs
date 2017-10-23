bbc5711cb8d77ddeecf4f181b2c9d569191aabce
WhynotPanda <bastian.heinzelmann@hotmail.de>
2017-10-23 13:39:24 +0200

    Hello

---
+++ Compilerbau/Hello.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Compilerbau
+{
+    class Hello
+    {
+        public static void Main()
+        {
+            Console.WriteLine("See ya, World!");
+            Console.ReadKey();
+        }
+    }
+}
