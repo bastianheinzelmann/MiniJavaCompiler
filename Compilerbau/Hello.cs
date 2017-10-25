7ea26d66a687e468431666c40d409a5138a13832
WhynotPanda <bastian.heinzelmann@hotmail.de>
2017-10-25 12:13:48 +0200

    added command line arguments :unicorn:

--- Compilerbau/Hello.cs
+++ Compilerbau/Hello.cs
@@ -2,25 +2,28 @@
 using Compilerbau.GeneratedParser;
 using System.Text;
 using Antlr4.Runtime;
+using System.IO;
 
 namespace Compilerbau
 {
     class Hello
     {
-        public static void Main()
+        public static void Main(String[] args)
         {
             try
             {
-                string input = "";
-                StringBuilder text = new StringBuilder();
-                Console.WriteLine("Input some shit");
+                //string input = "";
+                //StringBuilder text = new StringBuilder();
+                //Console.WriteLine("Input some shit");
 
-                while ((input = Console.ReadLine()) != "\u0004")
-                {
-                    text.AppendLine(input);
-                }
+                //while ((input = Console.ReadLine()) != "\u0004")
+                //{
+                //    text.AppendLine(input);
+                //}
 
-                AntlrInputStream inputStream = new AntlrInputStream(text.ToString());
+                string input = File.ReadAllText(args[0]);
+
+                AntlrInputStream inputStream = new AntlrInputStream(input);
                 MiniJavaLexer miniJavaLexer = new MiniJavaLexer(inputStream);
                 CommonTokenStream commonTokenStream = new CommonTokenStream(miniJavaLexer);
                 MiniJavaParser miniJavaParser = new MiniJavaParser(commonTokenStream);
@@ -30,8 +33,13 @@ namespace Compilerbau
             catch (Exception e)
             {
                 Console.WriteLine("Error: " + e);
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine("It's all my fault :(");
+                Console.ReadKey();
             }
 
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine("You did great!");
             Console.ReadKey();
         }
     }
